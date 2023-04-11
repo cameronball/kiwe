71f646940c19a41bce003494c8195f1bc56f6df2
cameron-ball <Camerondball@yahoo.com>
2023-04-11 08:56:57 +0100

    Initial profile page layout and basic style

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -20,4 +20,35 @@ router.get("/", (req, res, next) => {
   	res.status(200).render("profilePage", payload);
 })
 
+router.get("/:username", async (req, res, next) => {
+
+	var payload = await getPayload(req.params.username, req.session.user);
+
+  	res.status(200).render("profilePage", payload);
+})
+
+async function getPayload(username, userLoggedIn) {
+	var user = await User.findOne({ username: username})
+
+	if (user == null) {
+
+		user = await User.findById(username)
+
+		if (user == null) {
+			return {
+				pageTitle: "User not found",
+				userLoggedIn: userLoggedIn,
+				userLoggedInJs: JSON.stringify(userLoggedIn)
+			}
+		}
+	}
+
+	return {
+		pageTitle: "@"+user.username,
+		userLoggedIn: userLoggedIn,
+		userLoggedInJs: JSON.stringify(userLoggedIn),
+		profileUser: user
+	}
+}
+
 module.exports = router;
