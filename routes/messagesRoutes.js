e4e453df6e1b4d10e99942a5d81860f7312cda6b
cameron-ball <Camerondball@yahoo.com>
2023-10-18 19:40:06 +0100

    Create chat page

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const bodyParser = require("body-parser")
 const bcrypt = require('bcrypt'); 
 const User = require('../schemas/UserSchema');
+const Chat = require('../schemas/ChatSchema');
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -26,6 +27,28 @@ router.get("/new", (req, res, next) => {
 	};
 	
   	res.status(200).render("newMessage", payload);
+})
+
+router.get("/:chatId", async (req, res, next) => {
+
+	var userId = req.session.user;
+	var chatId = req.params.chatId;
+
+	var chat = await Chat.findOne({ _id: chatId, users: { $elemMatch: { $eq: userId } } })
+	.populate("users");
+
+	if(chat == null) {
+		// TODO
+	}
+
+	var payload = {
+		pageTitle: "Chat",
+		userLoggedIn: req.session.user,
+		userLoggedInJs: JSON.stringify(req.session.user),
+		chat: chat
+	};
+	
+  	res.status(200).render("chatPage", payload);
 })
 
 module.exports = router;
