f4a4dfb3416bd975c734f60e865c4c24b8f73e1f
cameron-ball <Camerondball@yahoo.com>
2023-10-18 21:33:13 +0100

    Chat name change modal & ajax call

---
+++ public/js/chatPage.js
@@ -0,0 +1,17 @@
+$("#chatNameButton").click(() => {
+	var name = $("#chatNameTextbox").val().trim();
+	
+	
+	$.ajax({
+		url: "/api/chats",
+		type: "PUT",
+		data: { chatName: name},
+		success: (data, status, xhr) => {
+			if(xhr.status != 204) {
+				alert("Could not update");
+			} else {
+				location.reload();
+			}
+		}
+	})
+});
