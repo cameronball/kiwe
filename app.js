df7b14358bd3afdcb817279728d44e532d1e85f4
cameron-ball <Camerondball@yahoo.com>
2023-10-20 12:53:58 +0100

    Install socket.io

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require("./database");
 const session = require('express-session');
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
+const io = require('socket.io')(server, { pingTimeout: 60000 });
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -61,3 +62,7 @@ app.get("/", middleware.requireLogin, (req, res, next) => {
 
   res.status(200).render("home", payload);
 })
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+});
