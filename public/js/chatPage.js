ced4cd10d23780de49f8cf6087c164dca4af0e27
cameron-ball <Camerondball@yahoo.com>
2023-10-20 13:56:06 +0100

    Show typing dots gif when someone is typing

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
 
 	socket.emit("join room", chatId);
-	socket.on("typing", () => console.log("Someone is typing"));
+	socket.on("typing", () => $(".typingDots").show());
 
 	$.get(`/api/chats/${chatId}`, (data) => {
 		$("#chatName").text(getChatName(data));
