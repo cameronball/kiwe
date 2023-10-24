456edf7ffce024f4faeb9a594c33effb809590b2
cameron-ball <Camerondball@yahoo.com>
2023-10-24 09:27:40 +0100

    Add requireAdmin middleware

--- middleware.js
+++ middleware.js
@@ -6,3 +6,12 @@ exports.requireLogin = (req, res, next) => {
     return res.redirect('/login');
   }
 }
+
+exports.requireAdmin = (req, res, next) => {
+  if (req.session && req.session.user && req.session.user.admin) {
+    return next();
+  }
+  else {
+    return res.redirect('/login');
+  }
+}
