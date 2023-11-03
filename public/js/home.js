b7233b8da7d1b80d5e10f27e4601091382e8b763
Cameron <49366907+cameronball@users.noreply.github.com>
2023-11-03 19:33:04 +0000

    Highlight home icon

--- public/js/home.js
+++ public/js/home.js
@@ -2,4 +2,5 @@ $(document).ready(() => {
 	$.get("/api/posts", { followingOnly: true }, results => {
 		outputPosts(results, $(".postsContainer"));
 	});
+	$("#homeButtonIcon").removeClass("far").addClass("fas");
 })
