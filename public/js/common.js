48b76f98cea582abb1b27cb88a6f5eea2c0cc62f
cameron-ball <Camerondball@yahoo.com>
2023-11-04 22:48:54 +0000

    Remove console.log statement in service worker registration

--- public/js/common.js
+++ public/js/common.js
@@ -10,7 +10,6 @@ $(document).ready(() => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/service-worker.js')
         .then(function(registration) {
-        console.log('Service Worker registered with scope: ', registration.scope);
         })
         .catch(function(err) {
         console.log('Service Worker registration failed: ', err);
