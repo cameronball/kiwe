657a0cc5ae60028c297c01e8a3d032b6b82d8769
cameron-ball <Camerondball@yahoo.com>
2023-11-04 13:40:17 +0000

    Add another advert

--- public/js/common.js
+++ public/js/common.js
@@ -580,9 +580,7 @@ var ads = [];
 // Add ads to array
 ads.push(`<div class="post" data-id="65463a609b3c16b8083cbd25">
 <div class="postActionContainer">
-    <span><i class="fas fa-ad" style="color:var(--blue);"></i>&nbsp;&nbsp;Paid promotion<span>
-    
-</span></span></div>
+    <span style="font-size:14px;"><i class="fas fa-ad" style="color:var(--blue);"></i>&nbsp;&nbsp;Paid promotion</span></div>
 <div class="mainContentContainer">
     <div class="userImageContainer">
         <img style="border-radius: 10%;" src="/uploads/images/cf1f01d79e5c0b82b2e25b8afb7d92e0.png" alt="Kiwe's Profile Image">
@@ -633,6 +631,50 @@ ads.push(`<div class="post" data-id="6546432f56a7bbae36bc7d0c">
         </div>    
         <div class="postBody">
             <span class="" style="">Why “Save for Later” when you can “Buy Now”? 🤔</span>
+        </div>
+        <div class="postFooter" style="">
+            <div class="postButtonContainer">
+                <button aria-label="Comment" data-toggle="modal" data-target="#replyModal">
+                    <i class="fas fa-comments"></i>
+                </button>
+            </div>
+            <div class="postButtonContainer green">
+                <button aria-label="Reshare" class="reshareButton ">
+                    <i class="fas fa-repeat"></i>
+                    <span></span>
+                </button>
+            </div>
+            <div class="postButtonContainer red">
+                <button aria-label="Like" class="likeButton ">
+                    <i class="far fa-heart"></i>
+                    &nbsp;<span></span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`);
+
+ads.push(`<div class="post" data-id="654645e0bbe171788dd7ada7">
+<div class="postActionContainer">
+    <span style="font-size:14px;"><i class="fas fa-ad" style="color:var(--blue);"></i>&nbsp;&nbsp;Paid promotion</span></div>
+<div class="mainContentContainer">
+    <div class="userImageContainer">
+        <img style="border-radius: 10%;" src="/uploads/images/54a36063d2976f634eb5ef48d45a75e6.png" alt="Google's Profile Image">
+    </div>
+    <div class="postContentContainer">
+        <div class="header">
+            <span><a href="/profile/google" class="displayName">Google</a><svg viewBox="0 0 22 22" data-toggle="tooltip" data-placement="top" title="" data-original-title="Verified Brand" style="height: 1.5em;padding-left:0px;vertical-align: -0.45em;"><rect width="10" height="10" x="6" y="6" fill="#000000"></rect><g><linearGradient id="12-a" gradientUnits="userSpaceOnUse" x1="4.411" x2="18.083" y1="2.495" y2="21.508"><stop offset="0" stop-color="#f4e72a"></stop><stop offset=".539" stop-color="#cd8105"></stop><stop offset=".68" stop-color="#cb7b00"></stop><stop offset="1" stop-color="#f4ec26"></stop><stop offset="1" stop-color="#f4e72a"></stop></linearGradient><linearGradient id="12-b" gradientUnits="userSpaceOnUse" x1="5.355" x2="16.361" y1="3.395" y2="19.133"><stop offset="0" stop-color="#f9e87f"></stop><stop offset=".406" stop-color="#e2b719"></stop><stop offset=".989" stop-color="#e2b719"></stop></linearGradient><g clip-rule="evenodd" fill-rule="evenodd"><path d="M13.324 3.848L11 1.6 8.676 3.848l-3.201-.453-.559 3.184L2.06 8.095 3.48 11l-1.42 2.904 2.856 1.516.559 3.184 3.201-.452L11 20.4l2.324-2.248 3.201.452.559-3.184 2.856-1.516L18.52 11l1.42-2.905-2.856-1.516-.559-3.184zm-7.09 7.575l3.428 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z" fill="url(#12-a)"></path><path d="M13.101 4.533L11 2.5 8.899 4.533l-2.895-.41-.505 2.88-2.583 1.37L4.2 11l-1.284 2.627 2.583 1.37.505 2.88 2.895-.41L11 19.5l2.101-2.033 2.895.41.505-2.88 2.583-1.37L17.8 11l1.284-2.627-2.583-1.37-.505-2.88zm-6.868 6.89l3.429 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z" fill="url(#12-b)"></path><path d="M6.233 11.423l3.429 3.428 5.65-6.17.038-.033-.005 1.398-5.683 6.206-3.429-3.429-.003-1.405.005.003z" fill="#d18800"></path></g></g></svg></span>
+            <span class="username">&nbsp;@google</span>
+            
+            
+            
+        </div>
+        
+        <div class="postBody">
+            <span class="" style="">Run into a friend? Just start talking and <a style="color:var(--blue);" href="https://kiwe.social/search/query/%23PixelBuds">#PixelBuds</a> Pro will use AI to automatically pause your audio and turn on transparency mode so you can focus on your conversation.
+
+<a style="color:var(--blue);" href="https://kiwe.social/search/query/%23MadeByGoogle">#MadeByGoogle</a></span>
         </div>
         <div class="postFooter" style="">
             <div class="postButtonContainer">
