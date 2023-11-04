6914799ab4c6e50c05d869582b9e41d7edccb5f7
cameron-ball <Camerondball@yahoo.com>
2023-11-04 22:39:34 +0000

    Add service worker and manifest file. (PWA Prep)

--- public/js/common.js
+++ public/js/common.js
@@ -5,7 +5,17 @@ var selectedUsers = [];
 
 $(document).ready(() => {
     refreshMessagesBadge();
-    refreshNotificationsBadge()
+    refreshNotificationsBadge();
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+        console.log('Service Worker registered with scope: ', registration.scope);
+        })
+        .catch(function(err) {
+        console.log('Service Worker registration failed: ', err);
+        });
+    }
 });
 
 $("#postTextarea, #replyTextarea").keyup(event => {
---
+++ public/js/serviceWorker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'kiwe-cache-v1';
+const urlsToCache = [
+  '/css/main.css',
+  '/css/login.css',
+  '/js/bootstrap.min.js',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
