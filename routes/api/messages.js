48e75389d3ee09309a1138fd55ce453235b0479c
Cameron <49366907+cameronball@users.noreply.github.com>
2024-03-28 12:14:23 +0000

    Update messages.js

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -9,6 +9,13 @@ const Chat = require('../../schemas/ChatSchema');
 const Message = require('../../schemas/MessageSchema');
 const Notification = require('../../schemas/NotificationSchema');
 
+const bodyParser = require("body-parser")
+const sanitizer = require('sanitizer');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const upload = multer({ dest: 'uploads/' });
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.post("/", async (req, res, next) => {
@@ -33,6 +40,59 @@ router.post("/", async (req, res, next) => {
 		.catch(error => console.log(error));
 
 		insertNotifications(chat, message);
+
+		res.status(201).send(message);
+	})
+	.catch(error => {
+		console.log(error);
+		res.sendStatus(400);
+	})
+});
+
+router.post("/imageMessage", upload.single("croppedImage"), async (req, res, next) => {
+	if(!req.body.chatId) {
+		console.log("Bad params sent with request");
+		return res.sendStatus(400);
+	}
+
+	if(req.file) {
+		var filePath = `/uploads/images/${req.file.filename}.png`;
+		var tempPath = req.file.path;
+		var targetPath = path.join(__dirname, `../../${filePath}`);
+
+		fs.rename(tempPath, targetPath, async error => {
+			if(error != null) {
+				console.log(error);
+				return res.sendStatus(400);
+			}
+		})
+
+		var includesImage = true;
+	}
+	else {
+		console.log("Image not included with request");
+		return res.sendStatus(400);
+	}
+
+	if (filePath == null) {
+		console.log("Image not included with request");
+		return res.sendStatus(400);
+	}
+
+	var newMessage = {
+		sender: req.session.user._id,
+		chat: req.body.chatId.
+		imageMessage: filePath;
+	};
+	
+	Message.create(newMessage)
+	.then(async message => {
+		message = await message.populate("sender");
+		message = await message.populate("chat");
+		message = await User.populate(message, { path: "chat.users" });
+
+		var chat = await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message })
+		.catch(error => console.log(error));
 
 		res.status(201).send(message);
 	})
