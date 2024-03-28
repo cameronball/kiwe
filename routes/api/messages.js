212d1558a994187f2ce2dc4bbc4ffcbf29cddffe
Cameron <49366907+cameronball@users.noreply.github.com>
2024-03-28 19:24:02 +0000

    Fix typo

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -80,7 +80,7 @@ router.post("/imageMessage", upload.single("croppedImage"), async (req, res, nex
 	var newMessage = {
 		sender: req.session.user._id,
 		chat: req.body.chatId,
-		imageMessage: filePath;
+		imageMessage: filePath,
 	};
 	
 	Message.create(newMessage)
