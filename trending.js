efb7567fa741513c99e43db73abecfa795ba38fe
Cameron <49366907+cameronball@users.noreply.github.com>
2024-04-02 14:29:26 +0100

    Update trending.js

--- trending.js
+++ trending.js
@@ -8,8 +8,11 @@ async function main() {
     const hashtagsFrequency = extractHashtagsFrequency(posts);
     console.log(hashtagsFrequency);
 
-    fs.writeFileSync('hashtagsFrequency.json', JSON.stringify(hashtagsFrequency, null, 2));
-    console.log('Hashtags frequency saved to file: hashtagsFrequency.json');
+    const publicDir = path.join(__dirname, 'public');
+    const filePath = path.join(publicDir, 'hashtagsFrequency.json');
+
+    fs.writeFileSync(filePath, JSON.stringify(hashtagsFrequency, null, 2));
+    console.log('Hashtags frequency saved to file:', filePath);
   } catch (error) {
     console.error('Error fetching posts:', error);
   }
