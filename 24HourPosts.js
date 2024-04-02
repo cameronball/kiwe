1e8693f1fc8d981e5afd93aeab4495996cdfb3e5
Cameron <49366907+cameronball@users.noreply.github.com>
2024-04-02 13:30:57 +0100

    Create 24HourPosts.js

---
+++ 24HourPosts.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const moment = require('moment');
+const Post = require('./schemas/PostSchema');
+
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function getPostsWithin24Hours() {
+  try {
+    const twentyFourHoursAgo = moment().subtract(24, 'hours');
+
+    const posts = await Post.find({
+      createdAt: { $gte: twentyFourHoursAgo },
+    }).exec();
+
+    console.log(posts);
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  } finally {
+    mongoose.disconnect();
+  }
+}
