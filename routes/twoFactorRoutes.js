a7e5a4b59993e1ac4f02efdf6eb63465d59ddf6c
Cameron <49366907+cameronball@users.noreply.github.com>
2024-04-05 19:36:57 +0100

    Update twoFactorRoutes.js

--- routes/twoFactorRoutes.js
+++ routes/twoFactorRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const bodyParser = require("body-parser")
 const bcrypt = require('bcrypt'); 
 const User = require('../schemas/UserSchema');
-
+const speakeasy = require("speakeasy");
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -15,6 +15,11 @@ router.get("/", (req, res, next) => {
   var payload = req.body;
   payload.pageTitle = "Two Factor Auth";
   res.status(200).render("twoFactor", payload);
+})
+
+router.post("/", async (req, res, next) => {
+  //-TODO
+  console.log("pass");
 })
 
 module.exports = router;
