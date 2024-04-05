151ca9a7fee906ca4ca26323617aea2a6cfc0356
Cameron <49366907+cameronball@users.noreply.github.com>
2024-04-05 18:21:52 +0100

    Update twoFactorRoutes.js

--- routes/twoFactorRoutes.js
+++ routes/twoFactorRoutes.js
@@ -17,44 +17,4 @@ router.get("/", (req, res, next) => {
   res.status(200).render("twofactor", payload);
 })
 
-router.post("/", async (req, res, next) => {
-
-  var payload = req.body;
-  payload.pageTitle = "Two Factor Auth";
-
-  username=req.body.logUsername.trim().toLowerCase();
-  password=req.body.logPassword.trim();
-
-  if (username && password) {
-    var user = await User.findOne({
-      $or: [
-        { username: username },
-        { email: username }
-      ]
-    })
-    .catch((error) => {
-      console.log(error);
-      payload.errorMessage = "Something went wrong.";
-      res.status(200).render("twofactor", payload);
-    })
-
-    if (user != null) {
-      var result = await bcrypt.compare(req.body.logPassword, user.password);
-
-      if (result === true) {
-        req.session.user = user;
-        req.session.user.twoFactorVerified = false;
-        return res.redirect("/");
-      }
-      
-    }
-
-    payload.errorMessage = "Login credentials incorrect.";
-    return res.status(200).render("twofactor", payload);
-  }
-
-  payload.errorMessage = "Make sure each field has a valid value.";
-  res.status(200).render("twofactor", payload);
-})
-
 module.exports = router;
