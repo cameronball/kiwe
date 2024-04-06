3d680e945204f7e5e5b6786f263b313dce4d8f8e
Cameron <49366907+cameronball@users.noreply.github.com>
2024-04-06 12:48:40 +0100

    Update twoFactorRoutes.js

--- routes/twoFactorRoutes.js
+++ routes/twoFactorRoutes.js
@@ -12,6 +12,15 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
+
+  if  (!req.session || !req.session.user) {
+    return res.redirect('/login');
+  }
+
+  if (req.session.user.twoFactorVerified) {
+    return res.redirect('/home');
+  }
+  
   var payload = req.body;
   payload.pageTitle = "Two Factor Auth";
   res.status(200).render("twoFactor", payload);
@@ -20,10 +29,46 @@ router.get("/", (req, res, next) => {
 router.post("/", async (req, res, next) => {
   var payload = req.body;
   payload.pageTitle = "Two Factor Auth";
+
+  if  (!req.session || !req.session.user) {
+    return res.redirect('/login');
+  }
+
+  if (req.session.user.twoFactorVerified) {
+    return res.redirect('/home');
+  }
   
   givenCode=req.body.twoFactorCode.trim();
-  console.log(givenCode);
-  console.log(req.session.user.username);
+
+  var user = await User.findOne({
+      $or: [
+        { username: req.session.user.username },
+        { email: req.session.user.email }
+      ]
+    })
+    .catch((error) => {
+      console.log(error);
+      payload.errorMessage = "Something went wrong.";
+      res.status(200).render("twoFactor", payload);
+    })
+
+   if (user != null) {
+      if (user.twoFactorSecret == null) {
+        payload.errorMessage = "Something went wrong.";
+        res.status(200).render("twoFactor", payload);
+      }
+      var verified = speakeasy.totp.verify({ secret: user.twoFactorSecret,
+                                       encoding: 'base32',
+                                       token: givenCode });
+      if (verified) {
+        req.session.user.twoFactorVerified = true;
+        return res.redirect('/home');
+      }
+      else {
+        payload.errorMessage = "Incorrect code.";
+        res.status(200).render("twoFactor", payload);
+      }
+    }
 
   res.status(200).render("twoFactor", payload);
 })
