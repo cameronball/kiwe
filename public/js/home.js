d3388029e21e0b35f6c29a8a09f3863c61f1a80d
Cameron <49366907+cameronball@users.noreply.github.com>
2024-04-16 10:15:01 +0100

    Test of trending page

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-	$.get("/api/posts", { followingOnly: true }, results => {
+	$.get("/api/posts", { trendingPage: true }, results => {
 		outputPosts(results, $(".postsContainer"));
 	});
 	$("#homeButtonIcon").removeClass("far").addClass("fas");
