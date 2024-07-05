44660d273325cc07a8fe33aa4b6e7b841370407c
Cameron <49366907+cameronball@users.noreply.github.com>
2024-07-05 14:09:25 +0100

    Update posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -321,7 +321,7 @@ async function getPosts(filter) {
             .populate("postedBy")
             .populate("reshareData")
             .populate("replyTo")
-	    .popu
+            .populate("likes") // Ensure that the likes field is populated with user details
             .sort({ createdAt: -1 });
 
         // Further populate the results
