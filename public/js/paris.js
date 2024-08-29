1386030c2784acfae980e01e169ad90765265992
Cameron <49366907+cameronball@users.noreply.github.com>
2024-08-29 14:16:29 +0100

    Create paris.js

---
+++ public/js/paris.js
@@ -0,0 +1,26 @@
+$(".sendMessageButton").click(() => {
+	messageSubmitted();
+});
+
+$(".inputTextbox").keydown((event) => {
+
+	if(event.which === 13 && !event.shiftKey) {
+		messageSubmitted();
+		return false
+	}
+});
+
+function messageSubmitted() {
+	var content = $(".inputTextbox").val().trim();
+
+	if(content != "") {
+		sendMessage(content);
+		$(".inputTextbox").val("");
+	}
+}
+
+function sendMessage(content) {
+	$.get("/api/chats/paris", { message: content }, (data, status, xhr) => {
+		console.log(data);
+	})
+}
