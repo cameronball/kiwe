83f28aa29bae2267e2a0223acc9c7a6bf546d36f
Cameron <49366907+cameronball@users.noreply.github.com>
2024-08-29 16:09:23 +0100

    Update paris.js

--- public/js/paris.js
+++ public/js/paris.js
@@ -54,8 +54,10 @@ function sendMessage(content) {
 	addChatMessageHtml(content, false);
 	$.get("/api/messages/paris", { message: content }, (data, status, xhr) => {
 		parisHistory.push({role: 'user', parts: [{ text: content }] });
+		localStorage.setItem("parisHistory", JSON.stringify(parisHistory));
 		addChatMessageHtml(data.response.candidates[0].content.parts[0].text, true);
 		parisHistory.push({role: 'model', parts: [{ text: data.response.candidates[0].content.parts[0].text }] });
+		localStorage.setItem("parisHistory", JSON.stringify(parisHistory));
 	})
 }
 
