4dcfefd16cb3fb9578faf15598e63a5e7c98dd63
Cameron <49366907+cameronball@users.noreply.github.com>
2024-08-29 14:04:42 +0100

    Update chats.js

--- routes/api/chats.js
+++ routes/api/chats.js
@@ -9,6 +9,22 @@ const Post = require('../../schemas/PostSchema');
 const Chat = require('../../schemas/ChatSchema');
 const Message = require('../../schemas/MessageSchema');
 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+require('dotenv').config();
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  generationConfig: {
+    candidateCount: 1,
+    maxOutputTokens: 8192,
+    temperature: 0.5,
+  },
+  systemInstruction: "You are Paris. You are a helpful chatbot on a micro-blogging social media site named Kiwe. Your aim is to assist users with any questions or queries they have or to just provide them with entertainment. You aren't yet able to do anything for them on the site such as posting or changing their settings, and requests such as these should simply be refused with a simple and friendly phrase such as, 'I'm sorry, I cannot do that yet, however, we are working on it hard and that will be available soon'. You can be as detailed and verbose as you want when necessary, but if it isn't necessary, then keep responses to the point and don't go off on tangents. You are encouraged to make occasional use of emojis where necessary, but don't overuse them.",
+});
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.post("/", async (req, res, next) => {
@@ -96,5 +112,18 @@ router.put("/:chatId/messages/markAsRead", async (req, res, next) => {
 		res.sendStatus(400);
 	})
 });
+
+router.get("/paris", async (req, res, next) => {
+	const message = req.body.message;
+	const chat = model.startChat({
+	history: [
+	   {
+	     role: "model",
+	     parts: [{ text: "Hi! I am Paris, your personal assistant here on Kiwe. What would you like to know or talk about today?" }],
+	   },
+	 ],
+	let result = await chat.sendMessage(message);
+	res.status(200).send(result.response.text());
+}
 
 module.exports = router;
