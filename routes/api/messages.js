b60f686902ddeb62428a96d9dc85f7ca84d81e43
Cameron <49366907+cameronball@users.noreply.github.com>
2024-08-30 13:14:01 +0100

    Update messages.js

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,4 +1,4 @@
-	const express = require('express');
+const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
@@ -12,11 +12,34 @@ const Notification = require('../../schemas/NotificationSchema');
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+];
+
+
 require('dotenv').config();
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API);
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
+  safetySettings,
   generationConfig: {
     candidateCount: 1,
     maxOutputTokens: 8192,
@@ -27,6 +50,7 @@ const model = genAI.getGenerativeModel({
 
 const secondModel = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
+  safetySettings,
   generationConfig: {
     candidateCount: 1,
     maxOutputTokens: 8192,
