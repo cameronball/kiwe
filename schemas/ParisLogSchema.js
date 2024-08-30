2edfb76bcd5df6367d89dc928b045f561450b978
Cameron <49366907+cameronball@users.noreply.github.com>
2024-08-30 18:54:45 +0100

    Update ParisLogSchema.js

--- schemas/ParisLogSchema.js
+++ schemas/ParisLogSchema.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const ChatSchema = new Schema({
+const ParisLogSchema = new Schema({
     request: { type: String, trim: true },
     sentBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
     response: { type: String, trim: true },
 }, { timestamps: true });
 
-module.exports = mongoose.model('Chat', ChatSchema);
+module.exports = mongoose.model('ParisLog', ParisLogSchema);
