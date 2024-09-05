628094aa69a204100b8cfb0c4c51113f2fe268c1
Cameron <49366907+cameronball@users.noreply.github.com>
2024-09-05 19:05:35 +0100

    Update bookmarks.js

--- routes/api/bookmarks.js
+++ routes/api/bookmarks.js
@@ -13,4 +13,22 @@ const upload = multer({ dest: 'uploads/' });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+router.put("/:id/bookmark", async (req, res, next) => {
+
+	var postId = req.params.id;
+	var userId = req.session.user._id;
+
+	var isBookmarked = req.session.user.bookmarks && req.session.user.bookmarks.includes(postId);
+
+	var option = isBookmarked ? "$pull" : "$addToSet";
+
+	req.session.user = await User.findByIdAndUpdate(userId, { [option]: { bookmarks: postId } }, { new: true })
+	.catch(error => {
+		console.log(error);
+		res.sendStatus(400);
+	});
+
+	res.status(200).send(post);
+})
+
 module.exports = router;
