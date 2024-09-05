d019250a70be01faae9cdc3d1dc51cfa5c986fd1
Cameron <49366907+cameronball@users.noreply.github.com>
2024-09-05 18:37:46 +0100

    Create bookmarksRoutes.js

---
+++ routes/bookmarksRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser")
+const bcrypt = require('bcrypt'); 
+const User = require('../schemas/UserSchema');
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+router.get("/", (req, res, next) => {
+	var payload = createPayload(req.session.user, "Bookmarks");
+  res.status(200).render("bookmarksPage", payload);
+});
+
+function createPayload(userLoggedIn, title){
+	return {
+		pageTitle: title,
+		userLoggedIn: userLoggedIn,
+		userLoggedInJs: JSON.stringify(userLoggedIn)
+	}
+}
+
+
+module.exports = router;
