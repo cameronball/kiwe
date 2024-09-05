fd31bf7f01c27dfb192e4538cae9e6d2945ad7f8
Cameron <49366907+cameronball@users.noreply.github.com>
2024-09-05 18:39:15 +0100

    Create bookmarks.js

---
+++ routes/api/bookmarks.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser")
+const sanitizer = require('sanitizer');
+const User = require('../../schemas/UserSchema');
+const Post = require('../../schemas/PostSchema');
+const Notification = require('../../schemas/NotificationSchema');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const upload = multer({ dest: 'uploads/' });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+module.exports = router;
