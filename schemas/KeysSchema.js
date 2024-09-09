0a2c3b2db87dff5d351cce35e30c2d632d9b7b37
Cameron <49366907+cameronball@users.noreply.github.com>
2024-09-09 15:27:49 +0100

    Create KeysSchema.js

---
+++ schemas/KeysSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ChatSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    key: { type: String, trim: true },
+    chat: { type: Schema.Types.ObjectId, ref: "Chat" },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Keys', KeysSchema);
