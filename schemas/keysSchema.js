661a894ff06faa44de874b480a066b9cc408f205
Cameron <49366907+cameronball@users.noreply.github.com>
2024-09-13 11:26:55 +0100

    Update and rename EncryptionKeysSchema.js to keysSchema.js

--- schemas/keysSchema.js
+++ schemas/keysSchema.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const ChatSchema = new Schema({
+const KeysSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User" },
     key: { type: String, trim: true },
     chat: { type: Schema.Types.ObjectId, ref: "Chat" },
 }, { timestamps: true });
 
-module.exports = mongoose.model('EncryptionKeys', EncryptionKeysSchema);
+module.exports = mongoose.model('Keys', KeysSchema);
