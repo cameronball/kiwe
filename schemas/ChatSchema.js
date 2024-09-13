010230819b28fe58b194b63f6d1563d5650c9c59
Cameron <49366907+cameronball@users.noreply.github.com>
2024-09-13 11:28:38 +0100

    Update ChatSchema.js

--- schemas/ChatSchema.js
+++ schemas/ChatSchema.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const ChatSchema = new Schema({
     chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
+    isEncrypted: { type: Boolean, default: true },
     users: [{ type: Schema.Types.ObjectId, ref: "User" }],
     latestMessage: { type: Schema.Types.ObjectId, ref: "Message" },
 }, { timestamps: true });
