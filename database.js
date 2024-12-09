3f9aa5471f0bc504b740ee1db8b28207aa2a70c3
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-09 15:27:02 +0000

    Update database.js

--- database.js
+++ database.js
@@ -1,23 +1,31 @@
+// Import necessary package
 const mongoose = require('mongoose');
 
+// Require .env secret file
 require('dotenv').config();
 
+// Create the database class
 class Database {
 
+  // On creation, connect to the database
   constructor() {
     this.connect();
   }
-  
+
+  // Connect method
   connect() {
-    console.log(process.env.MONGODB);
+    // Connect to the database using the secret string
     mongoose.connect(process.env.MONGODB)
     .then(() => {
+      // If successful, log as such
       console.log('Connected to MongoDB');
     })
     .catch((err) => {
+      // If there is an error, log it
       console.log('Failed to connect to MongoDB: ' + err);
     })
   }
 }
 
+// Create a new object of the database class
 module.exports = new Database();
