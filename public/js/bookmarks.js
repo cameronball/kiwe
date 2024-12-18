f811334bc3631f763b5836a003674a9de48b86b9
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-18 16:47:29 +0000

    Comments

--- public/js/bookmarks.js
+++ public/js/bookmarks.js
@@ -1,6 +1,10 @@
+// Code to be ran when the page has loaded
 $(document).ready(() => {
+	// Issue a get request for all posts bookmarked by the current user
 	$.get("/api/posts", { bookmarksOnly: true }, results => {
+		// Output the results to the postsContainer
 		outputPosts(results, $(".postsContainer"));
 	});
+	// Set the bookmarks icon in the navbar to be solid
 	$("#bookmarkButtonIcon").removeClass("far").addClass("fas");
 })
