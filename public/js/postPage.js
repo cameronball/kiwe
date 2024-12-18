076e8125828ad89cad5026ba4ba12317af6c557e
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-18 16:52:08 +0000

    Comments

--- public/js/postPage.js
+++ public/js/postPage.js
@@ -1,11 +1,17 @@
+// On load
 $(document).ready(() => {
+	// Send a get request to the post api endpoint 
 	$.get("/api/posts/" + postId, results => {
+		// Output the post as well as any replies
 		outputPostsWithReplies(results, $(".postsContainer"));
 	})
+	// On fail
 	.fail((xhr, status, error) => {
 		if (xhr.status === 404) {
+			// Output an error code
 			$(".postsContainer").append("<span class='noResults'>This post is not found or is longer available</span>");
 		}
 	  });
+	// Make the home icon solid
 	$("#homeButtonIcon").removeClass("far").addClass("fas");
 })
