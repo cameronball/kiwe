97690f218472dc379d43ec84d706a006f50158cb
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-20 02:08:07 +0000

    Update bannedRoutes.js

--- routes/bannedRoutes.js
+++ routes/bannedRoutes.js
@@ -5,16 +5,24 @@ const bodyParser = require("body-parser")
 const bcrypt = require('bcrypt'); 
 const User = require('../schemas/UserSchema');
 
+// Set the view engine to pug
 app.set("view engine", "pug");
+// Set the views directory
 app.set("views", "views");
 
+// Route to render the ban page
 router.get("/", (req, res, next) => {
+    // Create a payload object
 	var payload = createPayload(req.session.user, "Banned");
+    // Set the selected tab to banned
 	payload.selectedTab = "banned";
-  res.status(200).render("banPage", payload);
+    // Render the ban page with the payload
+  	res.status(200).render("banPage", payload);
 });
 
+// Function to create a payload object
 function createPayload(userLoggedIn, title){
+    // Return a payload object with the page title, user logged in, and user logged in as a JSON string
 	return {
 		pageTitle: title,
 		userLoggedIn: userLoggedIn,
@@ -22,5 +30,5 @@ function createPayload(userLoggedIn, title){
 	}
 }
 
-
+// Export the router
 module.exports = router;
