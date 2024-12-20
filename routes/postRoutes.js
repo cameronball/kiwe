b99684a4cf052536635937bef383e28d5708fb56
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-20 02:18:17 +0000

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,19 +5,24 @@ const bodyParser = require("body-parser")
 const bcrypt = require('bcrypt'); 
 const User = require('../schemas/UserSchema');
 
+// Set the view engine to pug
 app.set("view engine", "pug");
+// Set the views directory
 app.set("views", "views");
 
+// Route to render a specific post page
 router.get("/:id", (req, res, next) => {
-
+    // Create a payload object
 	var payload = {
 		pageTitle: "View post",
 		userLoggedIn: req.session.user,
 		userLoggedInJs: JSON.stringify(req.session.user),
-		postId: req.params.id
+		postId: req.params.id // Add the post ID to the payload
 	}
   
+    // Render the post page with the payload
   	res.status(200).render("postPage", payload);
 })
 
+// Export the router
 module.exports = router;
