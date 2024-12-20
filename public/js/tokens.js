99cfe5de521b8878cf9715ace7538464303c77fd
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-20 01:34:21 +0000

    Update tokens.js

--- public/js/tokens.js
+++ public/js/tokens.js
@@ -1,3 +1,5 @@
+// Code to be ran when the document is ready
 $(document).ready(() => {
+	// Remove the far class from the tokens button icon and add the fas class
 	$("#tokensButtonIcon").removeClass("far").addClass("fas");
 })
