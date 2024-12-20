1cd0e360e95ac9ce1095a117d437377bf6972feb
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-20 02:10:54 +0000

    Update logout.js

--- routes/logout.js
+++ routes/logout.js
@@ -7,13 +7,17 @@ const User = require('../schemas/UserSchema');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Route to handle user logout
 router.get("/", (req, res, next) => {
-
+    // If a session exists
 	if (req.session) {
+        // Destroy the session
 		req.session.destroy(() => {
+            // Redirect the user to the login page
 			res.redirect("/login");	
 		});
   	}
 })
 
+// Export the router
 module.exports = router;
