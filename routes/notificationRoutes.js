d37fb4a16fa5039d9605ffe5d37f7f9dc044ec2d
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-20 02:14:42 +0000

    Update notificationRoutes.js

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -7,17 +7,23 @@ const mongoose = require("mongoose");
 const User = require('../schemas/UserSchema');
 const Chat = require('../schemas/ChatSchema');
 
+// Set the view engine to pug
 app.set("view engine", "pug");
+// Set the views directory
 app.set("views", "views");
 
+// Route to render the notifications page
 router.get("/", (req, res, next) => {
+    // Create a payload object
 	var payload = {
 		pageTitle: "Notifications",
 		userLoggedIn: req.session.user,
 		userLoggedInJs: JSON.stringify(req.session.user)
 	};
 	
+    // Render the notifications page with the payload
   	res.status(200).render("notificationsPage", payload);
 })
 
+// Export the router
 module.exports = router;
