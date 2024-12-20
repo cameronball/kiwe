9d9e3f5451b2f9fd72b76634b228063da00cbe68
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-20 02:48:22 +0000

    Update sslRoutes.js

--- routes/sslRoutes.js
+++ routes/sslRoutes.js
@@ -6,12 +6,18 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 const User = require('../schemas/UserSchema');
 
+// Set the view engine to pug
 app.set("view engine", "pug");
+// Set the views directory
 app.set("views", "views");
 
+// Route to serve files from the .well-known directory
 router.get("/:path", (req, res, next) => {
+    // Sanitize the path parameter to prevent directory traversal
 	sanatizedPath = req.params.path.replace(/[&\/\\#, +()$~%'":*?<>{}]/g, '');
+    // Send the file from the .well-known directory
 	res.sendFile(path.join(__dirname, `../.well-known/${sanatizedPath}`));
 })
 
+// Export the router
 module.exports = router;
