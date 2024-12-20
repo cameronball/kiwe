68f739a795d75d4f89c686fd67fdc2140928c10b
Cameron <49366907+cameronball@users.noreply.github.com>
2024-12-20 01:34:42 +0000

    Update trending.js

--- public/js/trending.js
+++ public/js/trending.js
@@ -1,6 +1,10 @@
+// Code to be ran when the document is ready
 $(document).ready(() => {
+    // Send a get request to the /api/posts route, specifying that it is the trending page
 	$.get("/api/posts", { trendingPage: true }, results => {
+        // Output the posts to the postsContainer container
 		outputPosts(results, $(".postsContainer"));
 	});
+    // Remove the far class from the home button icon and add the fas class
 	$("#homeButtonIcon").removeClass("far").addClass("fas");
 })
